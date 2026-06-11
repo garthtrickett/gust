5d00c61505046a9a5440ceba7eaef1759198da95
Garth Trickett <garthtrickett@gmail.com>
2026-06-11 23:26:46 +1000

    fix: reorder codegen runtimes to declare os_Dir before function forward declarations

--- src/codegen.rs
+++ src/codegen.rs
@@ -457,6 +457,10 @@ impl Codegen {
             c_code.push_str("};\n\n");
         }
 
+        c_code.push_str(codegen_runtime::MOCK_PAYLOAD_RUNTIME);
+        c_code.push_str(codegen_runtime::FILE_IO_RUNTIME);
+        c_code.push_str(codegen_runtime::COLLECTIONS_RUNTIME);
+
         c_code.push_str("// ====================================================\n");
         c_code.push_str("// FUNCTION FORWARD DECLARATIONS\n");
         c_code.push_str("// ====================================================\n");
@@ -558,11 +562,6 @@ impl Codegen {
             }
         }
         c_code.push('\n');
-
-        c_code.push_str(codegen_runtime::MOCK_PAYLOAD_RUNTIME);
-        c_code.push_str(codegen_runtime::FILE_IO_RUNTIME);
-        c_code.push_str(codegen_runtime::COLLECTIONS_RUNTIME);
-
         c_code.push_str("// ====================================================\n");
         c_code.push_str("// DYNAMICALLY TRANSPILED USER STRUCTS\n");
         c_code.push_str("// ====================================================\n");
