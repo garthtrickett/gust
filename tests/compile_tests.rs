c86a5d5db0ea3d4a0f5db7b50d678185d5c0989e
Garth Trickett <garthtrickett@gmail.com>
2026-06-13 11:09:13 +1000

    feat: implement the next_token dispatch loop in Lexer and add token generation tests

--- tests/compile_tests.rs
+++ tests/compile_tests.rs
@@ -2466,6 +2466,86 @@ fn test_lexer_view_extraction() {
 }
 
 #[test]
+fn test_lexer_token_generation() {
+    let resolver = gust_lexer::resolver::ModuleResolver::new();
+    let fs_impl = gust_lexer::resolver::RealFileSystem;
+    let entry_path = std::path::Path::new("compiler/lexer_token_gen_entry.gst");
+
+    std::fs::create_dir_all("compiler").unwrap();
+
+    let entry_source = "
+        import \"token.gst\" as token;
+        import \"lexer.gst\" as lexer;
+        func main() {
+            mut ctx := os.Arena.New();
+            defer ctx.Free();
+            
+            mut l: lexer.Lexer[ctx];
+            lexer.init_lexer(&l, \"mut a := 10;\");
+            
+            mut t1 := lexer.next_token(&l);
+            os.LogInt(t1.token_type.tag);
+            os.LogStr(t1.literal);
+            
+            mut t2 := lexer.next_token(&l);
+            os.LogInt(t2.token_type.tag);
+            os.LogStr(t2.literal);
+            
+            mut t3 := lexer.next_token(&l);
+            os.LogInt(t3.token_type.tag);
+            os.LogStr(t3.literal);
+            
+            mut t4 := lexer.next_token(&l);
+            os.LogInt(t4.token_type.tag);
+            os.LogStr(t4.literal);
+            
+            mut t5 := lexer.next_token(&l);
+            os.LogInt(t5.token_type.tag);
+            os.LogStr(t5.literal);
+        }
+    ";
+    std::fs::write(&entry_path, entry_source).unwrap();
+
+    let res = resolver.resolve(&entry_path, &fs_impl);
+    assert!(res.is_ok(), "Module resolution failed: {:?}", res.err());
+
+    let (order, modules) = res.unwrap();
+    let mut checker = TypeChecker::new();
+    for path in &order {
+        if let Some(module) = modules.get(path) {
+            let stem = path.file_stem().unwrap().to_str().unwrap();
+            let is_entry = path == order.last().unwrap();
+            let prefix = if is_entry {
+                "".to_string()
+            } else {
+                format!("{}__", stem)
+            };
+            let check_res = checker.check_module(&module.program, &prefix);
+            assert!(check_res.is_ok(), "Typechecking failed: {:?}", check_res.err());
+        }
+    }
+
+    let codegen = Codegen::new(
+        checker.variable_types,
+        checker.struct_registry,
+        checker.function_registry,
+        checker.enum_registry,
+        checker.resolved_names,
+        checker.resolved_types,
+    );
+    let mut modules_for_codegen = Vec::new();
+    for path in &order {
+        if let Some(module) = modules.get(path) {
+            modules_for_codegen.push((path.clone(), module.program.clone()));
+        }
+    }
+    let c_output = codegen.generate(&modules_for_codegen);
+    assert!(c_output.contains("lexer__next_token("));
+
+    let _ = std::fs::remove_file(entry_path);
+}
+
+#[test]
 fn test_cli_dump_ast_integration() {
     use std::fs as std_fs;
     use std::process::Command;
